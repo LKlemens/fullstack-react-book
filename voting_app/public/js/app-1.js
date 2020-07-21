bfe6e3b94bff98dcdf1b8e9895e5eb72f3ac5856
LKlemens <klemens.lukaszczyk@gmail.com>
2020-07-21 10:21:36 +0200

    [voting-app] refactoring using transform-class-properties

--- voting_app/public/js/app-1.js
+++ voting_app/public/js/app-1.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
+  state = {
+    products: [],
+  };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id == productId) {
         return Object.assign({}, product, {
@@ -25,7 +19,7 @@ class ProductList extends React.Component {
     });
 
     this.setState({ products: nextProducts });
-  }
+  };
 
   render() {
     const products = this.state.products.sort((a, b) => b.votes > a.votes);
@@ -47,14 +41,8 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
+  handleUpVote = () => this.props.onVote(this.props.id);
 
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
   render() {
     return (
       <div className="item">
